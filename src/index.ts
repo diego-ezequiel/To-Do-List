7699c5b3093824fb324e9ede08cff75a24981717
diego <diego.rojas2001@hotmail.com>
2025-06-04 19:21:15 -0300

    añadiendo typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+const PUERTO = 3000
+
+app.get('/ping', (req, res) => {
+    console.log('alguien pingueó aquí!!')
+    res.send('pong')
+})
+
+app.listen(PUERTO, ()=>{
+    console.log('El servidor está corriendo en el puerto ${PUERTO}')
+})
