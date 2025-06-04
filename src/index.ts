ed3cecce8d4c38bd448c5b023ff9c2f015de478f
diego <diego.rojas2001@hotmail.com>
2025-06-04 19:35:53 -0300

    aprendiendo typescipt

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ app.use(express.json())
 
 const PUERTO = 3000
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
     console.log('alguien pingueó aquí!!')
     res.send('pong')
 })
 
 app.listen(PUERTO, ()=>{
-    console.log('El servidor está corriendo en el puerto ${PUERTO}')
+    console.log('El servidor está corriendo en el puerto ', PUERTO)
 })
